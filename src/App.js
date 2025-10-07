bc12346479b3061b4383f50acd3012b51d1891d2
Guilherme Soares <guihhsoaress@gmail.com>
2025-10-06 21:32:25 -0300

    NEW: Compontes da pagina

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import React, { useState } from "react";
-import './App.css'
+import { useState } from 'react';
+import Login from './components/Login/Login';
+import {Welcome} from "./components/Welcome/Welcome.js";
+import {Menu} from "./components/Menu/Menu.js";
+import {ContactList} from "./components/ContactList/ContactList.js";
+import {ContactForm} from "./components/ContactForm/ContactForm.js";
 
-import Login from './components/login/Login';
+import './App.css';
 
-function App(){
+function App() {
 
-const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [ isLoggedIn, setIsLoggedIn ] = useState(false);
+  const [ activeScreen, setActiveScreen] = useState("Welcome");
+  const [ contacts, setContacts] = useState(
+    {id:1, name: "Henrique Linhares", phone: "143234325"},
+    {id:2, name: "Valquiria Linhares", phone:"21242352"}
+  );
 
-const handleLogin = (userName, password) => {
-  if(userName === 'admin' && password === '123'){
-    setIsLoggedIn(true)
-  }
-  else{
-    alert("Usuário ou Senha inválidos")
-  }
+  const [contactToEdit, setContactToEdit] = useState(null);
+
+  function handleLogin( userName, password ) {
+    if(userName === "admin" && password === "123") {
+      setIsLoggedIn(true);
+    } else {
+      alert("Usuário ou senha inválida!!!")
+    }
+  };
+
+  const handleSaveContact = (contact) => {}
+  const handleDeleteContact = (id) => {}
+  const startEdit = (contact) => {}
+  const showCreateForm = () => {}
+  const handleNavigate = (screen) => {}
+
+  if (!isLoggedIn) {
+    return <Login onLogin={handleLogin}/>
+  };
+
+  return(
+    <div>
+      <Menu onNavigate={handleNavigate} onCreate={showCreateForm} />
+      <main className='content'>
+        {activeScreen === "Welcome" && <Welcome />}
+        {activeScreen === "list" && <ContactList contacts={contacts} onEdit={startEdit} onDelete={handleDeleteContact}/>}
+        {activeScreen === "form" && <ContactForm contactToEdit={contactToEdit} onSave={handleSaveContact} />}
+       
+      </main>
+    </div>
+  )
+
 }
 
-if(!isLoggedIn){
-  return <Login onLogin={handleLogin}/>
-}
-
-}export default App;
+export default App;
