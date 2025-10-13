8d4c32c206f8c4aa2c77dc7fff4e53b7b7c9d777
Guilherme Soares <guihhsoaress@gmail.com>
2025-10-13 19:03:51 -0300

    NEW: Componente ContactForm

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React from "react";
 import "./ContactList.css";
 
-const ContactList = ({contacts, onEdit, onDelete}) => {
+function ContactList({ contacts, onEdit, onDelete }) {
   return (
-    <div>
-
+    <div className="list-container">
+      <h2>Lista de Contato</h2>
+      {contacts.lenght > 0 ? (
+        <ul className="contact-list">
+          {contacts.map((contact) => (
+            <li key={contact.id} className="contact-item">
+              <div className="contact-info">
+                <span className="contact-name">{contact.name}</span>
+                <span className="contact-phone">{contact.phone}</span>
+              </div>
+              <div className="contact-actions">
+                <button
+                className="edit-btn"
+                  onClick={() => {
+                    onEdit(contact);
+                  }}
+                >
+                  Alterar
+                </button>
+                <button className="delete-btn"
+                  onClick={() => {
+                    onDelete(contact.id);
+                  }}
+                >
+                  Deletar
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="no-contacts">Nenhum contato cadastrado.</p>
+      )}
     </div>
-  )
+  );
 }
 
 export default ContactList;
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,12 +1,46 @@
-import React from 'react'
-import "./Menu.css"
+import React from "react";
+import "./Menu.css";
 
-const Menu = ({onNavigate, onCreate}) => {
+function Menu({ onNavigate, onCreate }) {
   return (
-    <div>
-
-    </div>
-  )
+    <nav className="menu">
+      <h3 className="menu-title">Agenda</h3>
+      <ul className="menu-list">
+        <li>
+          <button
+            className="menu-button"
+            onClick={() => {
+              onCreate();
+            }}
+          >
+            Cadastrar Contato
+          </button>
+        </li>
+        <li>
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              onNavigate("list");
+            }}
+          >
+            Lista de Contatos
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              onNavigate("welcome");
+            }}
+          >
+            Página Inicial
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Menu;
